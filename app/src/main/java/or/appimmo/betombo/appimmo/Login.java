c3f6736f60bae3d5524620146bee6835637d4629
betombomariot <gnartcodeur@gmail.com>
2018-02-01 16:26:23 +0300

    [Android-APPIMMO] fix somme controller on registarion

--- app/src/main/java/or/appimmo/betombo/appimmo/Login.java
+++ app/src/main/java/or/appimmo/betombo/appimmo/Login.java
@@ -2,11 +2,15 @@ package or.appimmo.betombo.appimmo;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -29,6 +33,17 @@ public class Login extends AppCompatActivity {
         TextView passwordView = findViewById(R.id.login_password_value);
         String password = passwordView.getText().toString();
         String email = emailView.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), R.string.enter_email_error, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),R.string.enter_password_error, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -36,10 +51,19 @@ public class Login extends AppCompatActivity {
                 if(task.isSuccessful())
                 startActivity(new Intent(Login.this,MainActivity.class));
                 else
-                    startActivity(new Intent(Login.this,Login.class));
-
+                {
+                   // startActivity(new Intent(Login.this, Login.class));
+                    EditText inputPassword = findViewById(R.id.login_password_value) ;
+                    EditText inputEmail = findViewById(R.id.login_email_value) ;
+                    inputPassword.setError(getString(R.string.error_login));
+                }
                 finish();
             }
         });
+    }
+
+    public void switchToRegister(View view)
+    {
+        startActivity(new Intent(this, Registration.class));
     }
 }
--- app/src/main/java/or/appimmo/betombo/appimmo/Registration.java
+++ app/src/main/java/or/appimmo/betombo/appimmo/Registration.java
@@ -2,10 +2,14 @@ package or.appimmo.betombo.appimmo;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -30,15 +34,39 @@ public class Registration extends AppCompatActivity {
         TextView emailView = findViewById(R.id.login_email_value);
         TextView passwordView = findViewById(R.id.login_password_value);
         String password = passwordView.getText().toString();
+
+
         String email = emailView.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), R.string.enter_email_error, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),R.string.enter_password_error, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-
+                if(task.isSuccessful())
                 startActivity(new Intent(Registration.this,MainActivity.class));
+                else {
+                    EditText inputPassword = findViewById(R.id.login_password_value);
+                    EditText inputEmail = findViewById(R.id.login_email_value);
+                    inputPassword.setError(getString(R.string.error_registration));
+                }
+                finish();
             }
         });
 
 
+    }
+
+    public void switchToLogin(View view)
+    {
+        startActivity(new Intent(this, Login.class));
     }
 }
